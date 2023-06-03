a03afadc4e46672aa734e83e1583ac3e3b78c861
Sowrato <sowrato1234@gmail.com>
2023-06-03 15:09:02 -0300

    Initial commit

---
+++ aula09/ex09.js
@@ -0,0 +1,9 @@
+
+
+var garota = 'Yasmim'
+console.log('Mano, to gostando da ' + garota)
+if (garota == 'Yasmim') {
+    console.log('Boah mano, garota linda pivete!')
+} else {
+    console.log('Calma ai irmão! Não é essa a sua garota, pensa com carinho ai vai!')
+}
